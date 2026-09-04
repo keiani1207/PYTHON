378d628575817a6ed5c2f5a57d6a5742bab8f4ac
Keiani <keiani1207@gmail.com>
2026-09-04 11:13:08 +0530

    code updating

---
+++ 8. tuples and sets in python.py
@@ -0,0 +1,22 @@
+# creating a tuple
+s = ("a","d","h","k")
+print(s)
+w = ("s",2,34.43)
+print(w)
+
+#creating a single tuple
+name =("sun",)
+print(name)
+
+#accessing tuple elements
+name =("sun","moon","star")
+print(name[1]) 
+print(name[-1])
+
+#slicing tuple
+b =("a","b","c","d","e","f")
+print(b[1:4])
+
+# checking its type
+b =("a","b","c","d","e","f")
+print(type(b))
