a13374d759eb2e89626f3aedfe3ff6ae87e7b03c
Keiani <keiani1207@gmail.com>
2026-09-07 18:41:47 +0530

    update

--- main.py
+++
@@ -1,8 +0,0 @@
-import keyword
-user_input =input("Enter variable name to check = ")
-if not user_input.isidentifier():
-    print("invalid variable name entered!!\nTry again..")
-elif keyword.iskeyword(user_input):
-    print(f"invalid!!. '{user_input}' is a reserved keyword in python")
-else:
-    print(f"'{user_input}' is valid variable name")
