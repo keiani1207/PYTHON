90f2ab57527f9c8c62fae767c8e3c18cf8cd07ba
Keiani <keiani1207@gmail.com>
2026-09-12 20:59:29 +0530

    update

--- 1. first line of code.py
+++ 1. first line of code.py
@@ -1,6 +1,6 @@
 # "print" function
 print("hello wrold!!")
-print("I will lead through my heart,not from head -")
+print("I will lead through my heart,not from head - pr")
 
 # Python is dynamically typed, example :
 a = 10
