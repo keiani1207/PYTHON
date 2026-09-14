2944c019373a1722fd30b44f3c6f16937da47ba9
Keiani <keiani1207@gmail.com>
2026-09-14 21:15:34 +0530

    update

--- pratice-problems.py
+++ pratice-problems.py
@@ -3,4 +3,4 @@ TipAmount=int(input("Tip Amount they wanna Give = "))
 Splitbill=int(input("Number of people who are splitting bill = "))
 FAP = TA+TipAmount
 x=FAP/Splitbill
-print(f"The Total Amount Should Be Paid is {FAP}. And Each Person Should Pay {x}. Cash or Card")
+print(f"The Total Amount Should Be Paid is {FAP}. And Each Person Should Pay {x}. Mode Of Payment is Cash or Card Sir??")
