169db3d0d2ee374c1797b3c2527458577b37c7ed
Keiani <keiani1207@gmail.com>
2026-09-14 21:06:29 +0530

    update

--- pratice-problems.py
+++ pratice-problems.py
@@ -1,3 +1,4 @@
-TA=float(input("Total Amount Of Bill To Pay = "))
+TA = float(input("Total Amount Of Bill To Pay = "))
 TipAmount=int(input("Tip Amount they wanna Give = "))
 splitbill=int(input("Number of people who are splitting bill = "))
+FAP = TA+TipAmount
