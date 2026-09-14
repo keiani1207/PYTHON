bb91cd98995e1d4121e3c53d41ef41a4b7753dbf
Keiani <keiani1207@gmail.com>
2026-09-14 21:35:13 +0530

    update

--- pratice-problems.py
+++ pratice-problems.py
@@ -1,3 +1,4 @@
+#Bill & Tip Calculator
 TA = float(input("Total Amount Of Bill To Pay = "))
 TipAm=int(input("Tip percentage they wanna Give = "))
 TipAmount=TA*(TipAm/100)
@@ -5,3 +6,4 @@ Splitbill=int(input("Number of people who are splitting bill = "))
 FAP = TA+TipAmount
 x=FAP/Splitbill
 print(f"The Total Amount Should Be Paid is {FAP}. And Each Person Should Pay {x}. Mode Of Payment is Cash or Card Sir??")
+
