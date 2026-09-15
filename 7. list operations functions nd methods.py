176fa7097e05d2f28dcb3b81fcc39bf8526166d3
Keiani <keiani1207@gmail.com>
2026-09-15 23:48:01 +0530

    update

--- 7. list operations functions nd methods.py
+++ 7. list operations functions nd methods.py
@@ -203,3 +203,8 @@ r=["p","h","y",list(("t","o","n"),)]
 print(len(r))
 print(r)
 print(r[0:3] + r[3])
+
+r=["p","h","y",list(("t","o","n"))]
+print(len(r))
+print(r)
+print(r[0:3] + r[3])
--- hh.py
+++
@@ -1,4 +0,0 @@
-r=["p","h","y",list(("t","o","n"),)]
-print(len(r))
-print(r)
-print(r[0:3] + r[3])
