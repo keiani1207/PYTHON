4c7ee92e970c241ed835b7ea5d78813ae5e5feff
keiani S <keiani1207@gmail.com>
2026-09-17 23:06:54 +0530

    Fix comment on startswith() example

--- 5. string data types.py
+++ 5. string data types.py
@@ -167,7 +167,7 @@ print("hi helloeveryone ,, 123".split(" ",1))
 
 # 30. startswith() - checks whether a string starts with a specified character or substring. It returns True if it starts with it, otherwise False.
 print("hello hi everyone".startswith("hello")) #true
-print(" hi hello hello everyone hello".startswith(" "))
+print(" hi hello hello everyone hello".startswith(" ")) #true
 print("hello everyone hello".startswith("e",6))
 
 # 31. strip() method removes leading and trailing characters (from both the left and right sides of a string) and returns the modified string.
