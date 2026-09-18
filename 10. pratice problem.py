11b9cb2585c28b49bb8f73dc73bae0e4d024763f
Keiani <keiani1207@gmail.com>
2026-09-18 20:15:56 +0530

    update

--- 10. pratice problem.py
+++ 10. pratice problem.py
@@ -25,5 +25,12 @@ y=input("enter the text or word = ")
 print(y in secret)
 print('x' not in secret)
 
-
+#Bitwise Manipulation Challenge
+a=12
+b=5
+print(a&b)
+print(a|b)
+print(a^b)
+print(a<<2)
+print(a>>1)
 
