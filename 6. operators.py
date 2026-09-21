6fbed745bcf04a962ad32e498c6ab01de5d09a37
keiani S <keiani1207@gmail.com>
2026-09-21 22:57:39 +0530

    Clarify membership operator output with a comment
    Added comment to clarify the output of the membership operator check.

--- 6. operators.py
+++ 6. operators.py
@@ -46,7 +46,7 @@ print(not(a>b))
 #membership operator
 my_list = [1,2,3,4,5]
 my_string ="python"
-print(3 in my_list)
+print(3 in my_list) #true
 print(7 in my_list)
 print(7 not in my_list)
 print("x" in my_string)
